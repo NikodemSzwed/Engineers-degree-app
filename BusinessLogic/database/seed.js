166d225361324aa71bb9d0822d15e67f61a0c995
Nikodem <nikodem.sd@gmail.com>
2025-09-03 20:49:38 +0200

    new seed

--- BusinessLogic/database/seed.js
+++ BusinessLogic/database/seed.js
@@ -319,7 +319,7 @@ async function seed() {
                 login: 'Administrator',
                 email: 'tak@wp.pl',
                 passwd: bcrypt.hashSync('ZAQ12wsx@#', 10),
-                PersonalSettings_json: `{"primaryColor":{"50":"{${color}.50}","100":"{${color}.100}","200":"{${color}.200}","300":"{${color}.300}","400":"{${color}.400}","500":"{${color}.500}","600":"{${color}.600}","700":"{${color}.700}","800":"{${color}.800}","900":"{${color}.900}","950":"{${color}.950}"},"darkMode":true,"history":["/alerts","/orders","/maps","/displays"]}`,
+                PersonalSettings_json: `{"history":["/alerts","/orders","/maps","/displays"]}`,
             },
             { login: 'Kierownik', email: 'tak2@wp.pl', passwd: bcrypt.hashSync('ZAQ12wsx@#', 10) },
         ]);
